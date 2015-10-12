2df15741589a9eb14d128f24d24c07eee90b6cef
caritosteph <carito.ulfe@gmail.com>
2015-10-11 20:34:54 -0500

    Linguita Client App

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularClient.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularClient
+ */
+angular.module('angularClient')
+    .controller('MainCtrl', function() {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
